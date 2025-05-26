3318c4651520bc17c4bd4389062203222db71043
Zain <zainhussaini9898@gmail.com>
2025-05-27 00:30:24 +0530

    changed meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,21 +13,21 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const baseUrl = "https://ghaffarenterprises.com/";
+const baseUrl = "https://www.iqrati.com/";
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
-  title: "Ghaffar Enterprises",
+  title: "Iqrati",
   openGraph: {
-    title: "Ghaffar Enterprises",
+    title: "Iqrati",
     images: [
       {
-        url: new URL("/logo_meta.png", baseUrl).toString(),
+        url: new URL("/ex2.png", baseUrl).toString(),
         width: 1200,
         height: 1200,
         alt: "Ghaffar Enterprises Square Logo",
       },
       {
-        url: new URL("/logo_meta.png", baseUrl).toString(),
+        url: new URL("/ex2.png", baseUrl).toString(),
         width: 1200,
         height: 630,
         alt: "Ghaffar Enterprises Rectangle Logo",
@@ -38,10 +38,10 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Ghaffar Enterprises",
+    title: "Iqrati",
     images: [
       {
-        url: new URL("/logo_meta.png", baseUrl).toString(),
+        url: new URL("/ex2.png", baseUrl).toString(),
         width: 1200,
         height: 1200,
         alt: "Ghaffar Enterprises Square Logo",
